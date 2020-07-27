ec723ddb3df21d76e84e6795002a15d9f3ad9b7b
roottech2020 <67610356+roottech2020@users.noreply.github.com>
2020-07-27 14:30:09 -0700

    Update scriptGame.js

--- scriptGame.js
+++ scriptGame.js
@@ -1,17 +1,18 @@
-let quotes = [
+/*let quotes = [
     "Genius is one percent inspiration and ninety-nine percent perspiration.", "You can observe a lot just by watching.","A house divided against itself cannot stand.",
     "Difficulties increase the nearer we get to the goal.","Fate is in your hands and no one elses"];
 
-    var rand;  
+    var rand;  */
     function setup(){
         createCanvas(windowWidth, windowHeight);
         background = (0,0,0);
-        rand = random(quotes.length);
+        //rand = random(quotes.length);
 
     }
 
    function draw(){
        fill(255,255,255);
        textSize(32);
-       text(""+quotes[rand], 1/8*width, 1/2*height);
+       text("test", 1/8*width, 1/2*height);
+       //text(""+quotes[rand], 1/8*width, 1/2*height);
    }
